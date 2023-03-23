88dc5d065d6a36f7810b572b41b6cf813f5e67fa
PeterNassrat <peternassrat90@gmail.com>
2023-03-23 18:12:01 +0200

    upload the repository files

---
+++ Project 1/Project1_Starter/src/CustomRectangle.java
@@ -0,0 +1,25 @@
+import java.awt.Rectangle;
+@SuppressWarnings("serial")
+public class CustomRectangle extends Rectangle {
+
+	public CustomRectangle(int x, int y, int width, int height) {
+        //ToDo
+    }
+
+    @Override
+    public String toString() {
+    	//ToDo
+    	return null;
+    }
+    
+    public boolean isInWBoxRect() {
+    	//ToDo
+    	return false;
+    }
+    
+    public boolean isValidRect() {
+    	//ToDo
+    	return false;
+    }
+
+}
---
+++ Project 1/Project1_Starter/src/Helper.java
@@ -0,0 +1,8 @@
+public class Helper {
+	public static boolean isValidName(String name) {
+		//ToDo
+		return false;
+	}
+
+	//Put any other needed methods
+}
