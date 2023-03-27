37ff37c3aa686542ccd940a1001f55e49d09104b
ranaosama4264 <ranaosama4264@gmail.com>
2023-03-28 01:00:45 +0200

    CustomRectangle

--- Project 1/Project1_Starter/src/CustomRectangle.java
+++ Project 1/Project1_Starter/src/CustomRectangle.java
@@ -5,21 +5,41 @@ public class CustomRectangle extends Rectangle {
 	public CustomRectangle(int x, int y, int width, int height) {
         //ToDo
     }
-
+  // override function to make this format 
     @Override
     public String toString() {
-    	//ToDo
-    	return null;
+    	
+    	return "("+ x +"," + y + "," + "width" + "height" + ")";
     }
     
-    public boolean isInWBoxRect() {
-    	//ToDo
-    	return false;
+    // isInWBoxRect function check if the rectangle in world box or not
+    public boolean isInWBoxRect()
+    {
+    
+    	if (x >= 0 && x+width <= 1024 && y >= 0 && y+height <= 1024) 
+    	{
+    		return true;
+		}
+    	else
+    	{
+    		return false;
+
+		}
     }
     
-    public boolean isValidRect() {
-    	//ToDo
-    	return false;
+    // isValidRect function check if height and width are greater than 0 or not
+    
+    public boolean isValidRect() 
+    {
+    	if (width>0&&height>0) 
+    	{
+			return true;
+		}
+    	else 
+    	{
+			return false;
+		}
+    	
     }
 
 }
